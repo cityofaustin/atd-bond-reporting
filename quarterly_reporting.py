ddd4fcb6a4cffd6da7516a15a4fd5f82d5a981e2
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2023-06-07 12:38:08 -0500

    documentation

--- quarterly_reporting.py
+++ quarterly_reporting.py
@@ -145,27 +145,34 @@ def encode_months(row):
 
 def to_output(df, spend):
     # Prepares the df for export by appending the spend plan info and calculating cumulative totals
+    # Melt the pivot table back down so the month columns become rows
     df = pd.melt(
         df.reset_index(),
         id_vars="unit_code",
         var_name="month_col",
         value_name="expenses",
     )
+    # merge the spend plan data in on the month_col
     df = pd.merge(df, spend, on=["unit_code", "month_col"], how="left")
+    # Get the text month name, all previous months to the quarter get grouped as Prior Spend
     df["Month Name"] = df.apply(decode_months, axis=1)
+    # Setting the start of the spend plan equal to the total of past expenses
     df["spend_plan"] = np.where(
         df["Month Name"] == "Prior Spend", df["expenses"], df["spend_plan"]
     )
 
-    # Cumulative totals
+    # Cumulative totals for the quarter for the three months
+    # Pulling the monthly expense data out for now
     quarter = df[df["month_col"] != "0"]
     quarter["spend_plan"] = quarter["spend_plan"].fillna(0)
     quarter = quarter.set_index("month_col")
     quarter = quarter.sort_index()
+    # Then calculate the cumulative total by FDU by month
     quarter["sum_expenses"] = quarter.groupby(["unit_code"])["expenses"].cumsum()
     quarter["sum_planned"] = quarter.groupby(["unit_code"])["spend_plan"].cumsum()
     quarter = quarter.reset_index()
 
+    # Then append the monthly expense data back in
     df = df[df["month_col"] == "0"]
     df = df.append(quarter)
 
@@ -200,13 +207,21 @@ def main():
     metadata = create_fdu_metadata_table(client)
     df_to_socrata(soda, metadata, FDU_DATASET, False)
 
-
+    # Get expenses data by FDU
     exp = get_data(client, EXPENSES)
+
+    # Now, create a column we will use to group by later of the form FY-MON
     exp["month_col"] = exp.apply(create_month_col, axis=1)
+
+    # Group by FDU, and calculate the expenses by each month
     exp = pd.pivot_table(
         exp, index="unit_code", columns="month_col", values="expenses", aggfunc="sum"
     )
     exp = exp.fillna(0)
+
+    # Now, unpivot the columns so that each row is a month for an FDU
+    # from: FDU, 22-OCT, 22-NOV, 22-DEC, ...
+    # to: FDU, month, expenses
     exp = pd.melt(
         exp.reset_index(),
         id_vars="unit_code",
@@ -214,23 +229,40 @@ def main():
         value_name="expenses",
     )
 
+    # Calculating fiscal quarter column based on the month
     exp["quarter"] = exp["month_col"].str[-2:].astype(int) // 3 + 1
+
+    # Extract FY data from month_col
     exp["fiscal_year"] = exp["month_col"].str[:4].astype(int)
+
+    # concat the fiscal year to the quarter number
     exp["fy-quarter"] = exp["fiscal_year"].astype(str) + exp["quarter"].astype(str)
+
+    # Calculate the current quarter (assumed latest)
     curr_quarter = exp["fy-quarter"].max()
+
+    # Returns a column where we check if each row is the current quarter
     exp["curr_quarter"] = exp.apply(select_quarter, axis=1, args=[curr_quarter])
 
+    # calculating previous quarter, if it's Q1 then prev is Q4 and all others
     if int(curr_quarter[-1:]) > 1:
         prev_quarter = f"{curr_quarter[:4]}{int(curr_quarter[-1:]) - 1}"
     else:
         prev_quarter = f"{int(curr_quarter[:4]) - 1}{3}"
 
+    # Extract only the previous quarter's data for the previous quarter tab
     prev_exp = exp[exp["fy-quarter"] <= prev_quarter]
 
+    # Make a pivot table where each row is an FDU and the columns are either 0 or the month of the selected quarter
+    # Like: FDU, 0, Jan-2023, Feb-2023, Mar-2023
+    # 0 is the previous expenses leading up to the quarter
     exp = pd.pivot_table(
         exp, index="unit_code", columns="curr_quarter", values="expenses", aggfunc="sum"
     )
+
     # Adding missing months from selected quarter
+    # if we only have a one or two months of data in the quarter we have to manually add zero expenses for them to
+    # show up.
     if len(exp.columns) < 4:
         for m in range(4 - len(exp.columns)):
             latest_month = exp.columns.max()
@@ -240,6 +272,7 @@ def main():
             else:
                 exp[f"{latest_month[:-2]}{added_month}"] = 0
 
+    # Now, do something similar for the previous quarter
     prev_exp["prev_quarter"] = prev_exp.apply(
         select_quarter, axis=1, args=[prev_quarter]
     )
@@ -251,19 +284,28 @@ def main():
         aggfunc="sum",
     )
 
+    # Gather spend plan data (originally in Google Drive)
     spend_plan = get_data(client, SPEND_PLAN)
     spend_plan["month_no"] = spend_plan.apply(encode_months, axis=1)
+    # Make the same month_col, so we can match it back to the expenses data
     spend_plan["month_col"] = (
         spend_plan["fiscal_year"].astype(str) + spend_plan["month_no"]
     )
 
+    # Merging the spend plan into the expenses for current quarter
     exp = to_output(exp, spend_plan)
+    # Creating column for selecting the quarter in Power BI
     exp["quarter_selection"] = f"{curr_quarter[:4]} Q{curr_quarter[-1:]}"
+
+    # Merging the spend plan into the expenses for previous quarter
     prev_exp = to_output(prev_exp, spend_plan)
+    # Creating column for selecting the quarter in Power BI
     prev_exp["quarter_selection"] = f"{prev_quarter[:4]} Q{prev_quarter[-1:]}"
 
+    # Append the previous expenses to the current expenses
     output = exp.append(prev_exp)
 
+    # Replace data in Socrata
     df_to_socrata(soda, output, EXPENSES_DATASET, False)
 
 
